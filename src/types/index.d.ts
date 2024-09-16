f3e2cc9d1f8309ff2abc7fd8c77b1378f70b0919
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-09-16 13:10:50 +0600

    fix: elsint run for dts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,5 @@
 import type { AuthUser } from '@/modules/auth/auth.types';
-import type { EnvType } from '@/utils/catchEnvValidation';
+import { EnvType } from '@/utils/catchEnvValidation';
 
 declare global {
   namespace Express {
@@ -8,6 +8,8 @@ declare global {
     }
   }
   namespace NodeJS {
-    interface ProcessEnv extends EnvType {}
+    interface ProcessEnv extends EnvType {
+      [key: string]: string | undefined;
+    }
   }
 }
