28212944490d5c0a63a40c06ab34cecc172dc6bb
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-09-23 11:59:37 +0600

    fix: eslint issue fixed

--- src/modules/auth/auth.test.ts
+++ src/modules/auth/auth.test.ts
@@ -24,63 +24,55 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
 
     // Register Validation Error test
     it('Validation Error Test for register', async () => {
-      const expected = {
-        status: 400,
-        success: false,
-        message: /required/i,
-        type: /validation error/i,
-      };
+      const expected = { status: 400, success: false, type: /validation error/i };
+
       // check name validation
       const notNameBody = omit(body, 'name');
-      expected.message = /name/i;
-      await apiTester({ url, method: 'post', body: notNameBody, expected });
+      let resBody = await apiTester({ url, method: 'post', body: notNameBody, expected });
+      expect(resBody?.message).toMatch(/name/i);
 
       // check email validation
       const notEmailBody = omit(body, 'email');
+      resBody = await apiTester({ url, method: 'post', body: notEmailBody, expected });
+      expect(resBody?.message).toMatch(/email/i);
+
       const invalidEmailBody = { ...body, email: 'abraham' };
-      expected.message = /email/i;
-      await apiTester({ url, method: 'post', body: notEmailBody, expected });
-      await apiTester({ url, method: 'post', body: invalidEmailBody, expected });
+      resBody = await apiTester({ url, method: 'post', body: invalidEmailBody, expected });
+      expect(resBody?.message).toMatch(/email/i);
 
       // check password validation
       const notPassBody = omit(body, 'password');
+      resBody = await apiTester({ url, method: 'post', body: notPassBody, expected });
+      expect(resBody?.message).toMatch(/password/i);
+
       const invalidPassBody = { ...body, password: '123456' };
-      expected.message = /password/i;
-      await apiTester({ url, method: 'post', body: notPassBody, expected });
-      await apiTester({ url, method: 'post', body: invalidPassBody, expected });
+      resBody = await apiTester({ url, method: 'post', body: invalidPassBody, expected });
+      expect(resBody?.message).toMatch(/password/i);
     });
 
     // Register Successful test
     it('Register Successful test with all proper data', async () => {
-      const expectedUser: Partial<TUser> = {
+      const user: Partial<TUser> = {
         ...body,
         role: 'user',
         _id: types.string,
         createdAt: types.string,
         updatedAt: types.string,
       };
-      delete expectedUser.password;
+      delete user.password;
 
-      const expected = {
-        status: 201,
-        success: true,
-        data: { token: types.string, user: expectedUser },
-      };
+      const expected = { status: 201, success: true };
 
-      const { body: resBody } = await apiTester({ url, method: 'post', body, expected });
-      token = resBody.data.token;
+      const resBody = await apiTester({ url, method: 'post', body, expected });
+      expect(resBody?.data).toStrictEqual(expect.objectContaining({ token: types.string, user }));
+      token = resBody?.data.token;
     });
 
     // Duplication errors test
     it('Duplication errors test for Register', async () => {
-      const expected = {
-        status: 400,
-        success: false,
-        message: /email/i,
-        type: /app error/i,
-      };
-
-      await apiTester({ url, method: 'post', body, expected });
+      const expected = { status: 400, success: false, type: /app error/i };
+      const resBody = await apiTester({ url, method: 'post', body, expected });
+      expect(resBody?.message).toMatch(/email/i);
     });
   });
 
@@ -91,51 +83,46 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
 
     // login Validation Error test
     it('Validation Error Test for login', async () => {
-      const expected = {
-        status: 400,
-        success: false,
-        message: /required/i,
-        type: /validation error/i,
-      };
+      const expected = { status: 400, success: false, type: /validation error/i };
 
       // check email validation
       const notEmailBody = omit(body, 'email');
+      let resBody = await apiTester({ url, method: 'post', body: notEmailBody, expected });
+      expect(resBody?.message).toMatch(/email/i);
+
       const invalidEmailBody = { ...body, email: 'abraham' };
-      expected.message = /email/i;
-      await apiTester({ url, method: 'post', body: notEmailBody, expected });
-      await apiTester({ url, method: 'post', body: invalidEmailBody, expected });
+      resBody = await apiTester({ url, method: 'post', body: invalidEmailBody, expected });
+      expect(resBody?.message).toMatch(/email/i);
 
       // check password validation
       const notPassBody = omit(body, 'password');
+      resBody = await apiTester({ url, method: 'post', body: notPassBody, expected });
+      expect(resBody?.message).toMatch(/password/i);
+
       const invalidPassBody = { ...body, password: '123456' };
-      expected.message = /password/i;
-      await apiTester({ url, method: 'post', body: notPassBody, expected });
-      await apiTester({ url, method: 'post', body: invalidPassBody, expected });
+      resBody = await apiTester({ url, method: 'post', body: invalidPassBody, expected });
+      expect(resBody?.message).toMatch(/password/i);
     });
 
     // not found user and password not match test
     it('Not found user and password not match test for login', async () => {
-      const expected = {
-        status: 404,
-        success: false,
-        message: /not found/i,
-        type: /app error/i,
-      };
+      const expected = { status: 404, success: false, type: /app error/i };
 
       // not found user test
       const notFoundEmailBody = { ...body, email: 'abraham1@gmail.com' };
-      await apiTester({ url, method: 'post', body: notFoundEmailBody, expected });
+      let resBody = await apiTester({ url, method: 'post', body: notFoundEmailBody, expected });
+      expect(resBody?.message).toMatch(/not found/i);
 
       // password not matched test
       expected.status = 400;
-      expected.message = /password/i;
       const notMatchedPassBody = { ...body, password: '123456@Aa1' };
-      await apiTester({ url, method: 'post', body: notMatchedPassBody, expected });
+      resBody = await apiTester({ url, method: 'post', body: notMatchedPassBody, expected });
+      expect(resBody?.message).toMatch(/password/i);
     });
 
     // login Successful test
     it('login Successful test with all proper data', async () => {
-      const expectedUser = {
+      const user = {
         name: testUser.name,
         email: body.email,
         role: expectEnum(userRoles),
@@ -144,13 +131,10 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
         updatedAt: types.string,
       };
 
-      const expected = {
-        status: 200,
-        success: true,
-        data: { token: types.string, user: expectedUser },
-      };
+      const expected = { status: 200, success: true };
 
-      const { body: resBody } = await apiTester({ url, method: 'post', body, expected });
+      const resBody = await apiTester({ url, method: 'post', body, expected });
+      expect(resBody?.data).toStrictEqual(expect.objectContaining({ token: types.string, user }));
       token = resBody.data.token;
     });
   });
@@ -165,17 +149,15 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
 
     // change password Validation Error test
     it('Authorization Test for change-password', async () => {
-      const expected = {
-        status: 401,
-        success: false,
-        message: /invalid token/i,
-        type: /unauthorized access/i,
-      };
+      const expected = { status: 401, success: false, type: /unauthorized access/i };
 
-      await apiTester({ url, method: 'post', body, expected });
+      let resBody = await apiTester({ url, method: 'post', body, expected });
+      expect(resBody?.message).toMatch(/invalid token/i);
+
       // check invalid token validation
       const invalidToken = 'eyJhbGciOiJIUzI';
-      await apiTester({ url, method: 'post', body, expected, token: invalidToken });
+      resBody = await apiTester({ url, method: 'post', body, expected, token: invalidToken });
+      expect(resBody?.message).toMatch(/invalid token/i);
     });
 
     // change password Validation Error test
@@ -183,42 +165,45 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
       const expected = {
         status: 400,
         success: false,
-        message: /required/i,
         type: /validation error/i,
       };
 
       // check currentPassword validation
-      const notCurrPassBody = omit(body, 'currentPassword');
-      const invalidCurrPassBody = { ...body, currentPassword: '12345678' };
-      expected.message = /currentPassword/i;
-      await apiTester({ url, method: 'post', body: notCurrPassBody, expected, token });
-      await apiTester({ url, method: 'post', body: invalidCurrPassBody, expected, token });
+      const notCurrPass = omit(body, 'currentPassword');
+      let resBody = await apiTester({ url, method: 'post', body: notCurrPass, expected, token });
+      expect(resBody?.message).toMatch(/currentPassword/i);
+
+      const invalidCurrPass = { ...body, currentPassword: '12345678' };
+      resBody = await apiTester({ url, method: 'post', body: invalidCurrPass, expected, token });
+      expect(resBody?.message).toMatch(/currentPassword/i);
 
       // check newPassword validation
       const notNewPassBody = omit(body, 'newPassword');
+      resBody = await apiTester({ url, method: 'post', body: notNewPassBody, expected, token });
+      expect(resBody?.message).toMatch(/newPassword/i);
+
       const invalidNewPassBody = { ...body, newPassword: '12345678' };
-      expected.message = /newPassword/i;
-      await apiTester({ url, method: 'post', body: notNewPassBody, expected, token });
-      await apiTester({ url, method: 'post', body: invalidNewPassBody, expected, token });
+      resBody = await apiTester({ url, method: 'post', body: invalidNewPassBody, expected, token });
+      expect(resBody?.message).toMatch(/newPassword/i);
 
       // check now app errors
       expected.type = /app error/i;
 
       // check same pass validation
       const samePassBody = { ...body, newPassword: body.currentPassword };
-      expected.message = /same/i;
-      await apiTester({ url, method: 'post', body: samePassBody, expected, token });
+      resBody = await apiTester({ url, method: 'post', body: samePassBody, expected, token });
+      expect(resBody?.message).toMatch(/same/i);
 
       // password not matched
       expected.status = 400;
-      expected.message = /password/i;
       const notMatchPassBody = { ...body, currentPassword: '123456@Aa100' };
-      await apiTester({ url, method: 'post', body: notMatchPassBody, expected, token });
+      resBody = await apiTester({ url, method: 'post', body: notMatchPassBody, expected, token });
+      expect(resBody?.message).toMatch(/password/i);
     });
 
     // change password Successful test
     it('change password Successful test with all proper data', async () => {
-      const expectedUser = {
+      const user = {
         name: testUser.name,
         email: testUser.email,
         role: expectEnum(userRoles),
@@ -227,13 +212,11 @@ describe(`Auth apis test, API = ${baseUrl}`, () => {
         updatedAt: types.string,
       };
 
-      const expected = {
-        status: 200,
-        success: true,
-        data: { token: types.string, user: expectedUser },
-      };
+      const expected = { status: 200, success: true };
 
-      await apiTester({ url, method: 'post', body, expected, token });
+      const resBody = await apiTester({ url, method: 'post', body, expected, token });
+      expect(resBody?.data).toStrictEqual(expect.objectContaining({ token: types.string, user }));
+      token = resBody.data.token;
     });
   });
 });
--- src/utils/test.api.ts
+++ src/utils/test.api.ts
@@ -3,31 +3,27 @@ import supertest from 'supertest';
 
 export const request = supertest(app);
 
-type ArrayOrObject = Record<string, unknown> | Array<unknown>;
-
-type Matcher<T extends ArrayOrObject> = {
+type Matcher = {
   status?: number;
   success?: boolean;
-  data?: T;
-  message?: string | RegExp;
   type?: string | RegExp;
 };
 
-type TesterProps<T extends ArrayOrObject> = {
+type TesterProps = {
   url: string;
   method?: 'get' | 'post' | 'put' | 'patch' | 'delete';
   body?: Record<string, unknown>;
-  expected?: Matcher<T>;
+  expected?: Matcher;
   token?: string;
 };
 
-const apiTester = async <T extends ArrayOrObject>({
+const apiTester = async ({
   url,
   method = 'get',
   body: reqBody,
-  expected: { status = 200, success = true, message, type, data } = {},
+  expected: { status = 200, success = true, type } = {},
   token,
-}: TesterProps<T>) => {
+}: TesterProps) => {
   const query = request[method](url);
   if (reqBody) {
     query.send(reqBody);
@@ -38,19 +34,11 @@ const apiTester = async <T extends ArrayOrObject>({
   const res = await query;
   expect(res.status).toBe(status);
   expect(res.body?.success).toBe(success);
-  if (data && Array.isArray(data)) {
-    expect(res.body?.data).toStrictEqual(expect.arrayContaining(data));
-  } else if (data && Object.keys(data).length) {
-    expect(res.body?.data).toStrictEqual(expect.objectContaining(data));
-  }
-  if (message) {
-    expect(res.body?.message).toMatch(message);
-  }
   if (type) {
     expect(res.body?.type).toMatch(type);
   }
 
-  return { body: res.body };
+  return res.body;
 };
 
 export default apiTester;
