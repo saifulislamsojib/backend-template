30382d9e5b25e63582d61599025f2f7b2a22b811
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-03 11:37:56 +0600

    fix(logger-in-test): logger set silent in test env

--- src/configs/test.setup.ts
+++ src/configs/test.setup.ts
@@ -3,8 +3,6 @@ import { testDbConnect, testDbDisconnect } from './test.db';
 
 // set env for test
 process.env.NODE_ENV = 'test';
-process.env.LOG_LEVEL = 'silent';
-process.env.IS_LOGS_ON_FILE = 'false';
 
 beforeAll(() => Promise.all([testDbConnect(), redisClient.connect()]), 70 * 1000);
 
