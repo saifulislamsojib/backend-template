ef5d976df11b002f6cd9bab6888b5c950878bb12
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-04 22:42:14 +0600

    test(test-db): test db connect timeout updated

--- src/configs/test.setup.ts
+++ src/configs/test.setup.ts
@@ -4,6 +4,6 @@ import { testDbConnect, testDbDisconnect } from './test.db';
 // set env for test
 process.env.NODE_ENV = 'test';
 
-beforeAll(() => Promise.all([testDbConnect(), redisClient.connect()]), 70 * 1000);
+beforeAll(() => Promise.all([testDbConnect(), redisClient.connect()]), 70 * 100000);
 
 afterAll(() => Promise.all([testDbDisconnect(), redisClient.disconnect()]));
