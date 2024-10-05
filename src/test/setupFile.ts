9035fab4ef09641b00d7d6a5e792d6b77ef3e76a
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-05 12:20:38 +0600

    test(test-setup): test setup updated

--- src/test/setupFile.ts
+++ src/test/setupFile.ts
@@ -1,9 +1,13 @@
 import { dbConnect, dbDisconnect } from '../configs/db';
-import redisClient from '../configs/redis';
+// import redisClient from '../configs/redis';
 
 // set env for test
 process.env.NODE_ENV = 'test';
 
-beforeAll(() => Promise.all([dbConnect(), redisClient.connect()]), 70 * 1000);
+// beforeAll(() => Promise.all([dbConnect(), redisClient.connect()]), 70 * 1000);
 
-afterAll(() => Promise.all([dbDisconnect(), redisClient.disconnect()]));
+// afterAll(() => Promise.all([dbDisconnect(), redisClient.disconnect()]));
+
+beforeAll(dbConnect, 70 * 1000);
+
+afterAll(dbDisconnect);
