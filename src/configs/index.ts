0fb8ff09d7ecbad0ddd334e2fbe0d75a540acb09
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-05 11:57:33 +0600

    test(test-setup): test setup updated

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -10,7 +10,7 @@ const configs = {
   node_env: process.env.NODE_ENV,
   bcrypt_salt_rounds: parseInt(process.env.BCRYPT_SALT_ROUNDS, 10),
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
   log_level: process.env.LOG_LEVEL,
   is_logs_on_file: process.env.IS_LOGS_ON_FILE === 'true',
   api_route: '/api/v1',
