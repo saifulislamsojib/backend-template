161f7ed0d602cc384308b74531e2f5dbe25da0e1
saifulislamsojib <saifulislam.sojib56@gmail.com>
2025-01-31 18:34:47 +0600

    feat(mongodb-test): test mongodb updated

--- src/test/setupFile.ts
+++ src/test/setupFile.ts
@@ -1,10 +1,5 @@
 import { dbConnect, dbDisconnect } from '../configs/db';
-// import redisClient from '../configs/redis';
 
-// beforeAll(() => Promise.all([dbConnect(), redisClient.connect()]), 70 * 1000);
-
-// afterAll(() => Promise.all([dbDisconnect(), redisClient.disconnect()]));
-
-beforeAll(dbConnect, 70 * 1000);
+beforeAll(dbConnect, 5000);
 
 afterAll(dbDisconnect);
